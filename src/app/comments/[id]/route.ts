ffefdfcab9b580ee0b6f9155c6781706906974cc
Dharyl Carry S. Almora <37977733+dharylovey@users.noreply.github.com>
2024-03-15 23:27:47 +0800

    GET POST PATCH and DELETE

---
+++ src/app/comments/[id]/route.ts
@@ -0,0 +1,35 @@
+import { comments } from "../data";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  return Response.json(comment);
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const { text } = body;
+  const idx = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  comments[idx].text = text;
+  return Response.json(comments[idx]);
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+
+  const idx = comments.findIndex( comment => comment.id === parseInt(params.id) );
+  const deletedComment = comments[idx]
+  comments.splice(idx, 1);
+  return Response.json(deletedComment);
+}
